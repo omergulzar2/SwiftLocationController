96f8a351be647d10ebb83f29cfd95f9735777da6
Omer Gulzar <you@example.com>
2019-05-29 21:39:21 -0400

    Intial Project Setup

---
+++ SwiftLocationController/SwiftLocationController.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftLocationController.swift
+//  SwiftLocationController
+//
+//  Created by Omer Gulzar on 5/29/19.
+//  Copyright © 2019 Sumaiya Inc. All rights reserved.
+//
+
+class SwiftLocationController {
+
+}
